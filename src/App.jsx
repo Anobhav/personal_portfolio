7a5617f2ec255436ac7ef8f5339a8924fda6cdd2
Anobhav <singhanobhav@gmail.com>
2026-06-19 16:27:46 +0530

    Homepage Component Breakdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import AboutMe from './components/About_me'
+import ContactMe from './components/Contact_me';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+import { Navbar } from './components/Navbar';
 export default function App() {
   return (
-    <>
-    </>
+    <div className='bg-slate-900 text-white font-Space-Grostesk min-h-screen'>
+      <Navbar/>
+      <AboutMe/>
+      <Experience/>
+      <Education/>
+      <ContactMe/>
+      <Footer/>    
+    </div>
   );
 }
---
+++ src/components/About_me.jsx
@@ -0,0 +1,7 @@
+export default function AboutMe(){
+    return(
+        <>
+            <h1>aboutme</h1>
+        </>
+    )
+};
